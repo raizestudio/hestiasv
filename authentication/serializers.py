95d5618ce97005c984da47d357022e9bbc6cf50f
Joel <raizetvision@gmail.com>
2024-06-30 15:54:35 +0200

    feat: implement session and authentication handle

--- authentication/serializers.py
+++ authentication/serializers.py
@@ -1,10 +1,24 @@
 from rest_framework import serializers
 
-from authentication.models import Token
+from authentication.models import Refresh, Session, Token
 
 
 class TokenSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Token
+        fields = "__all__"
+
+
+class RefreshSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Refresh
+        fields = "__all__"
+
+
+class SessionSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Session
         fields = "__all__"
