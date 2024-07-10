3d9672efc70a0c469d58f0207c674c47ee07ea6b
Joel <70848930+raizestudio@users.noreply.github.com>
2024-07-10 23:00:11 +0200

    improvements

--- user/policies.py
+++ user/policies.py
@@ -4,12 +4,15 @@ from rest_access_policy import AccessPolicy
 class BaseUserAccessPolicy(AccessPolicy):
     statements = [
         {"action": ["list"], "principal": "*", "effect": "allow", "condition": "is_admin"},
+        {"action": ["retrieve_dashboard"], "principal": "*", "effect": "allow", "condition": "is_admin"},
         # {"action": ["publish", "unpublish"], "principal": ["group:editor"], "effect": "allow"},
         # {"action": ["destroy"], "principal": ["*"], "effect": "allow", "condition": "is_author"},
         # {"action": ["*"], "principal": ["*"], "effect": "deny", "condition": "is_happy_hour"},
     ]
 
     def is_admin(self, request, view, action) -> bool:
+        if request.user.is_anonymous:
+            return False
         return request.user.role.code == "RO-ADM" or request.user.role.code == "RO-MNG"
 
     # def is_author(self, request, view, action) -> bool:
--- user/views.py
+++ user/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from rest_access_policy import AccessViewSetMixin
 from rest_framework import status, viewsets
+from rest_framework.decorators import action
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 
@@ -21,8 +22,27 @@ class UserViewSet(AccessViewSetMixin, viewsets.ModelViewSet):
     serializer_class = UserSerializer
     permission_classes = [AllowAny]
 
-    def list(self, request, *args, **kwargs):
+    def list(self, request, *args, **kwargs) -> Response:
         return super().list(request, *args, **kwargs)
+
+    @action(detail=False, methods=["get"], url_path="retrieve-dashboard", url_name="retrieve_dashboard")
+    def retrieve_dashboard(self, request, *args, **kwargs) -> Response:
+        user = request.user
+
+        if user.is_anonymous:
+            return Response(
+                {"detail": "You must be logged in to access this resource"},
+                status=status.HTTP_401_UNAUTHORIZED,
+            )
+
+        dashboard = {
+            "new_users": User.objects.filter(role__code="RO-USR").count(),
+        }
+
+        if user.role.group.code == "GR-ADM":
+            return Response(dashboard, status=status.HTTP_200_OK)
+
+        return Response({"detail": "No dashboard for you."}, status=status.HTTP_200_OK)
 
 
 class GroupViewSet(viewsets.ModelViewSet):
@@ -30,7 +50,7 @@ class GroupViewSet(viewsets.ModelViewSet):
     serializer_class = GroupSerializer
     permission_classes = [AllowAny]
 
-    def list(self, request, *args, **kwargs):
+    def list(self, request, *args, **kwargs) -> Response:
         return super().list(request, *args, **kwargs)
 
 
@@ -39,6 +59,6 @@ class RoleViewSet(viewsets.ModelViewSet):
     serializer_class = RoleSerializer
     permission_classes = [AllowAny]
 
-    def list(self, request, *args, **kwargs):
+    def list(self, request, *args, **kwargs) -> Response:
         roles = self.serializer_class(self.queryset, many=True).data
         return Response(roles, status=status.HTTP_200_OK)
